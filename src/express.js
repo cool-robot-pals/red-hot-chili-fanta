e1be033ea9d8926056611f9437f4d29200e25317
Lau Skeeter <lawr@lauragonzalez.cc>
2018-05-20 16:10:28 +0100

    move tweet info to app

--- src/express.js
+++ src/express.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const maker = require('./product-maker');
+const maker = require('./make-post');
 
 app.use(express.static('assets'));
 app.use('/emoji', express.static('node_modules/twemoji/2/svg'));
