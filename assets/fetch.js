137737b01abeb59c47cf6437245b353d408f89ab
Lau Skeeter <lawr@lauragonzalez.cc>
2018-05-20 21:44:58 +0100

    bigger label, phase out cssom

--- assets/fetch.js
+++ assets/fetch.js
@@ -8,9 +8,7 @@ const fetcher = async () => {
 
 const go = async () => {
 	const data = await fetcher();
-
 	const product = data.product;
-
 	const rotation = 5 - Math.random() * 10;
 
 	document.querySelector('x-fanta x-label').innerHTML = product.name
@@ -18,24 +16,22 @@ const go = async () => {
 		.reduce((a, b) => a.concat(b), [])
 		.map(_ => `<span>${_}</span>`)
 		.join('');
+
 	document.querySelector('x-fanta .edible').src = `/emoji/${
 		product.edible
 	}.svg`;
-	document
-		.querySelector('x-fanta')
-		.attributeStyleMap.set(
-			'background-color',
-			`rgb(${product.hero._rgb.join()})`
-		);
-	document
-		.querySelector('x-fanta')
-		.attributeStyleMap.set('transform', `rotate(${rotation}deg)`);
-	document
-		.querySelector('body')
-		.attributeStyleMap.set(
-			'background-color',
-			`rgb(${product.palette[0]._rgb.join()})`
-		);
+
+	document.querySelector(
+		'x-fanta'
+	).style.backgroundColor = `rgba(${product.hero._rgb.map(Math.floor).join()})`;
+
+	document.querySelector('x-fanta').style.transform = `rotate(${rotation}deg)`;
+
+	document.querySelector(
+		'body'
+	).style.backgroundColor = `rgba(${product.palette[0]._rgb
+		.map(Math.floor)
+		.join()})`;
 
 	console.log(JSON.stringify(data));
 };
